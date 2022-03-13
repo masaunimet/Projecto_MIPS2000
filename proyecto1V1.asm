de4baae9c09ffb1f108bcc24a12ccfb601a456df
Tony Cunsolo <t27302013@correo.unimet.edu.ve>
2022-03-13 01:12:36 -0400

    resta listo

--- proyecto1V1.asm
+++ proyecto1V1.asm
@@ -10,92 +10,126 @@ syscall
 .end_macro
 
 .macro llenar_ceros(%cadena)
-li $t9 0x30
-li $t8 0
+li indice 0
 loop_llenar:
-	bgt $t8 49 fin_loop_llenar
-	sb $t9 %cadena($t8)
+	bgt indice 49 fin_loop_llenar
+	sb $s0 %cadena(indice)
 	
-	addi $t8 $t8 1
+	addi indice indice 1
 	b loop_llenar
 	
 fin_loop_llenar:
-addi $t8 $t8 1
-sb $zero %cadena($t8)
+addi indice indice 1
+sb $zero %cadena(indice)
 .end_macro
 
 .macro llenar_ceros2(%cadena)
-li $t9 0x30
-li $t8 0
+li indice 0
 loop_llenar:
-	bgt $t8 50 fin_loop_llenar2
-	sb $t9 %cadena($t8)
+	bgt indice 50 fin_loop_llenar2
+	sb $s0 %cadena(indice)
 	
-	addi $t8 $t8 1
+	addi indice indice 1
 	b loop_llenar
 	
 fin_loop_llenar2:
-addi $t8 $t8 1
-sb $zero %cadena($t8)
+addi indice indice 1
+sb $zero %cadena(indice)
 .end_macro
 
 .macro convertir_numero(%cadena)
-li $t9 0
-li $t8 0
+li indice 0
+li indice2 0
+li tamano2 0
 loop:
 	
-	lb $t7 %cadena($t9)
+	lb digito1 %cadena(indice)
 	
-	beqz $t7 fin_loop
-	beq $t7 0x0A llenar_ceros
+	beqz digito1 fin_loop
+	beq digito1 0x0A llenar_ceros
 	
-	bgt $t9 50 inicio
-	blt $t7 0x30 inicio
-	bgt $t7 0x39 inicio
+	bgt indice 50 inicio
+	blt digito1 0x30 inicio
+	bgt digito1 0x39 inicio
  
-	addi $t8 $t8 1
-	addi $t9 $t9 1
+	addi indice2 indice2 1
+	addi indice indice 1
+	addi tamano2 tamano2 1
 	
 	b loop
 	
 	llenar_ceros: 
 		 
-		 beq $t9 50 llenar_ceros_part2
-		 li $t7 0x30 
-		 sb $t7 %cadena($t9)
+		 beq indice 50 llenar_ceros_part2
+		 li digito1 0x30 
+		 sb digito1 %cadena(indice)
 		  
-		 addi $t9 $t9 1 
+		 addi indice indice 1 
 		 b llenar_ceros
 		 
 	llenar_ceros_part2:
 	
-	li $t9 49
-	subi $t8 $t8 1
+	li indice 49
+	subi indice2 indice2 1
 		
 	loop_ceros:
 		
-		bltz $t8 fin_loop
+		bltz indice2 fin_loop
 		
-		lb $t7 %cadena($t8)
-		sb $s0 %cadena($t8)
-		sb $t7 %cadena($t9)
+		lb digito1 %cadena(indice2)
+		sb $s0 %cadena(indice2)
+		sb digito1 %cadena(indice)
 			
-		subi $t9 $t9 1
-		subi $t8 $t8 1
+		subi indice indice 1
+		subi indice2 indice2 1
 		
 		b loop_ceros
 		
 fin_loop:
 .end_macro
 
+.macro comparar_tamanos #compara si el primero es el mas grande o no
+bgt tamano1 tamano2 mas_grande
+blt tamano1 tamano2 mas_peque
+
+iguales:
+subi tamano1 tamano1 50
+subi tamano2 tamano2 50
+mul tamano1 tamano1 -1
+mul tamano2 tamano2 -1
+
+loop_comparativo:
+	
+	bgt tamano1 49 mas_peque
+	
+	lb digito1 espacionumero1(tamano1)
+	lb digito23 espacionumero2(tamano2)
+	
+	bgt digito1 digito23 mas_grande
+	blt digito1 digito23 mas_peque
+	
+	addi tamano1 tamano1 1
+	addi tamano2 tamano2 1
+	
+	b loop_comparativo
+
+mas_grande:
+li boleano 1
+b fin_comparar_tamanos
+	
+mas_peque:
+li boleano 0
+
+fin_comparar_tamanos:
+.end_macro
 
 .data
 mensaje: .asciiz "Ingrese operacion a realizar \n1. Sumar\n2. Restar\n3. Multiplicar\n4. Salir\n"
 salto: .asciiz "\n"
 mensajeerror : .asciiz "ingrese una opcion del 1 al 4"
 primernumero: .asciiz "Ingrese el primer numero: "
-segundonumero: .asciiz "Ingrese el segundo numero: "
-resultado: .asciiz "El resultado es"
+segundonumero: .asciiz "Ingrese el segundo numero:"
+resultado: .asciiz "El resultado es: "
 espacionumero1: .space 51
 espacionumero2: .space 51
 espacionumero3: .space 52
@@ -107,7 +141,9 @@ espacionumero3: .space 52
 .eqv digito23 $t2
 .eqv boleano $t3 
 .eqv indice $t4 
-.eqv indice3 $t5
+.eqv indice2 $t5
+.eqv tamano1 $t6 
+.eqv tamano2 $t7
 
 li $s0 0x30
 li $s1 0x39
@@ -122,7 +158,7 @@ print_string(mensaje)
 
 li $v0 5
 syscall
-move $t0 $v0
+move opcion $v0
 
 blt opcion 1 inicio
 bgt opcion 4 inicio
@@ -144,11 +180,12 @@ li $a1 51
 syscall
 print_string(salto)
 convertir_numero(espacionumero1)
+move tamano1 tamano2
 convertir_numero(espacionumero2)
 
 li boleano 0
 li indice 49
-li indice3 50
+li indice2 50
 
 beq opcion 1 sumar
 beq opcion 2 restar
@@ -175,9 +212,9 @@ sumar:
 	
 	continue2:
 	add digito23 digito23 $s0
-	sb digito23 espacionumero3(indice3)
+	sb digito23 espacionumero3(indice2)
 	subi indice indice 1
-	subi indice3 indice3 1
+	subi indice2 indice2 1
 	b sumar
 	
 fin_sumar:
@@ -185,14 +222,91 @@ beqz boleano no_condicional
 li digito23 0x31
 sb digito23 espacionumero3($zero)
 no_condicional:
-print_string(espacionumero1)
 print_string(salto)
-print_string(espacionumero2)
-print_string(salto)
+print_string(resultado)
 print_string(espacionumero3)
-b salir
+print_string(salto)
+print_string(salto)
+b inicio
 
 restar:
+comparar_tamanos
+beqz boleano segundo_mayor
+li indice 49
+li indice2 50
+
+	primer_mayor:
+	li boleano 0
+	
+	loop_resta:
+		
+		blt indice 0 fin_resta
+		lb digito1 espacionumero1(indice)
+		lb digito23 espacionumero2(indice)
+		
+		beqz boleano no_condicional_resta1
+		li boleano 0
+		subi digito1 digito1 1
+		
+		no_condicional_resta1:
+		sub digito1 digito1 $s0
+		sub digito23 digito23 $s0
+		
+		sub digito23 digito1 digito23
+		
+		bgez digito23 normal1
+		li boleano 1 
+		addi digito23 digito23 10
+		
+		normal1:
+		add digito23 digito23 $s0
+		sb digito23 espacionumero3(indice2)
+		
+		subi indice indice 1
+		subi indice2 indice2 1
+		
+		b loop_resta
+
+	segundo_mayor:
+		
+		blt indice 0 fin_resta_especial
+		lb digito1 espacionumero1(indice)
+		lb digito23 espacionumero2(indice)
+		
+		beqz boleano no_condicional_resta2
+		li boleano 0
+		subi digito23 digito23 1
+		
+		no_condicional_resta2:
+		sub digito1 digito1 $s0
+		sub digito23 digito23 $s0
+		
+		sub digito23 digito23 digito1
+		
+		bgt digito23 -1 normal2
+		li boleano 1 
+		addi digito23 digito23 10
+		
+		normal2:
+		add digito23 digito23 $s0
+		sb digito23 espacionumero3(indice2)
+		
+		subi indice indice 1
+		subi indice2 indice2 1
+		
+		b segundo_mayor
+
+fin_resta_especial:
+li digito23 0x2D
+sb digito23 espacionumero3($zero)
+
+fin_resta:
+print_string(salto)
+print_string(resultado)
+print_string(espacionumero3)
+print_string(salto)
+print_string(salto)
+b inicio
 
 multiplicar:
 
